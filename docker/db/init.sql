34c472e4710875c91005a542e4d2e15efd9bebe9
VladSebov <sebov.vlad@inbox.ru>
2026-06-05 22:44:08 +0300

    build: добавить скрипт для создания таблиц в docker compose

---
+++ docker/db/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE IF NOT EXISTS employees (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    middle_name VARCHAR(50),
+    position VARCHAR(100),
+    department VARCHAR(100),
+    hire_date DATE DEFAULT CURRENT_DATE
+    );
